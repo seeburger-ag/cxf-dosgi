359f671fa7de43678e3aba692ed98869ac5b5fa2
Johannes Utzig <j.utzig@seeburger.de>
2016-03-04 21:30:33 +0100

    fixed copyright headers

--- dsw/fabric-dosgi/src/test/java/io/fabric8/dosgi/ExtraInvocationTest.java
+++ dsw/fabric-dosgi/src/test/java/io/fabric8/dosgi/ExtraInvocationTest.java
@@ -1,9 +1,17 @@
-/*
- * ExtraInvocationTest.java
+/**
+ *  Copyright 2016 SEEBURGER AG
  *
- * created at 28.02.2016 by utzig <j.utzig@seeburger.de>
+ *  SEEBURGER licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
  *
- * Copyright (c) SEEBURGER AG, Germany. All Rights Reserved.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
  */
 package io.fabric8.dosgi;
 
@@ -16,7 +24,6 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
 
-import org.apache.cxf.dosgi.dsw.handlers.ConfigTypeHandlerTracker;
 import org.fusesource.hawtdispatch.Dispatch;
 import org.fusesource.hawtdispatch.DispatchQueue;
 import org.junit.After;
